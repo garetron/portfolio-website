c49335eddd7ce12ae82f767e1efd02f001beed32
Gareth Hobbs <GarethHobbs01@Gmail.com>
2022-11-16 16:50:20 -0500

    Add CSS and Jquery for hiding header on scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,16 @@ if (viewportWidth >= 640) {
 }
 
 
+var prev = 0;
+var $window = $(window);
+var nav = $('header');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
+
+
+
+
