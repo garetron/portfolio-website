15c856d8cb7482eda350a3eb93d7e0380d19f5f2
Gareth Hobbs <GarethHobbs01@Gmail.com>
2022-11-16 15:26:01 -0500

    Add hamburger menu and javascript interaction

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-console.log("Hello world.")
+console.log("Thanks for checking out my code! - Gareth Hobbs")
+
+
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle' && viewportWidth <= 640) {
+        x.className += ' responsive'; //add class name with space "_" to seperate addition
+        
+    } else {
+        x.className = 'navtoggle';
+    }
+}
+
+
+var viewportWidth = $( window ).width();
+var navTog = document.getElementById('myNavtoggle');
+var responsiveTog = document.getElementsByClassName('navtogg');
+
+console.log("The viewport is " + String(viewportWidth) + "px")
+
+
+if (viewportWidth >= 640) {
+    navTog.style.width = null;
+    navTog.style.marginLeft = null;
+    console.log("removed the style tags from nav");
+} else {
+    navTog.style.width = String(viewportWidth) + "px";
+    navTog.style.marginLeft = "-16px";
+    console.log("set max width for nav");
+}
+
+
