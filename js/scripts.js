69a9a6bf64a3fbc96e4b2e61182ffaed6fb889c8
Gareth Hobbs <GarethHobbs01@Gmail.com>
2022-11-21 15:33:28 -0500

    adjust nav html structure and about image
    remove javascript for nav and create full width class that contains padding for elements. Create rules to limit the size of about image to 500px across breakpoints.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-console.log("Thanks for checking out my code! - Gareth Hobbs")
+console.log("Thanks for checking out my code! - Gareth Hobbs");
 
 
 // Toggle the hamburger menu as visible or not
 
+var viewportWidth = $( window ).width();
+
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle' && viewportWidth <= 640) {
+    if (x.className === 'navtoggle' && viewportWidth < 640) {
         x.className += ' responsive'; //add class name with space "_" to seperate addition
         
     } else {
@@ -14,25 +16,6 @@ function menuToggle() {
 }
 
 
-var viewportWidth = $( window ).width();
-var navTog = document.getElementById('myNavtoggle');
-var responsiveTog = document.getElementsByClassName('navtogg');
-
-console.log("The viewport is " + String(viewportWidth) + "px")
-
-
-// Set the navigation dropdown to screen width below 640px for Hamburger menu
-// Strip style tags above 640
-
-if (viewportWidth >= 640) {
-    navTog.style.width = null;
-    navTog.style.marginLeft = null;
-    console.log("removed the style tags from nav");
-} else {
-    navTog.style.width = String(viewportWidth) + "px";
-    navTog.style.marginLeft = "-16px";
-    console.log("set max width for nav");
-}
 
 
 // Hide or show header on scrolling - animation duration set in header CSS
