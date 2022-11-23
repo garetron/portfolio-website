cd1843921efafd0f05928c00987276e6cdec36ba
Gareth Hobbs <GarethHobbs01@Gmail.com>
2022-11-23 11:05:20 -0500

    Lint code in project

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,26 @@
 console.log("Thanks for checking out my code! - Gareth Hobbs");
 
-
 // Toggle the hamburger menu as visible or not
 
-var viewportWidth = $( window ).width();
+var viewportWidth = $(window).width();
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle' && viewportWidth < 640) {
-        x.className += ' responsive'; //add class name with space "_" to seperate addition
-        
-    } else {
-        x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle" && viewportWidth < 640) {
+    x.className += " responsive"; //add class name with space "_" to seperate addition
+  } else {{
+    x.className = "navtoggle";
+  }
 }
-
-
-
 
 // Hide or show header on scrolling - animation duration set in header CSS
 
 var prev = 0;
 var $window = $(window);
-var nav = $('header');
+var nav = $("header");
 
-$window.on('scroll', function(){
+$window.on("scroll", function () {
   var scrollTop = $window.scrollTop();
-  nav.toggleClass('hidden', scrollTop > prev);
+  nav.toggleClass("hidden", scrollTop > prev);
   prev = scrollTop;
 });
-
-
-
-
