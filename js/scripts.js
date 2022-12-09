60c461d9ecf8244a20d3b0c1143156cc1b94caa2
Gareth Hobbs <GarethHobbs01@Gmail.com>
2022-12-09 10:18:52 -0500

    change circle

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,31 @@ console.log("Thanks for checking out my code! - Gareth Hobbs");
 
 var viewportWidth = $(window).width();
 
+var mouseX = 0,
+  mouseY = 0,
+  xp = 0,
+  yp = 0;
+
+$("#hero").mousemove(function (e) {
+  mouseX = e.pageX - 30;
+  mouseY = e.pageY - 30;
+
+  xp += (mouseX - xp) / 12;
+  yp += (mouseY - yp) / 12;
+
+  $(".circle").css({
+    display: "block",
+    left: xp + "px",
+    top: yp + "px",
+  });
+});
+
+$("#work").mouseenter(function () {
+  $(".circle").css({
+    display: "none",
+  });
+});
+
 function menuToggle() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle" && viewportWidth < 640) {
@@ -19,6 +44,7 @@ var prev = 0;
 var $window = $(window);
 var nav = $("header");
 
+// ADD COLORS ON HOVER ABOVE PAGE
 $(".fun-heading").hover(
   function () {
     // $(".logo").addClass("fun-heading-always");
@@ -28,6 +54,7 @@ $(".fun-heading").hover(
     $("#hero").addClass("fun-bg-always");
     $(".b-or-w").addClass("b-or-w-always");
   },
+  // REMOVE COLORS AFTER HOVER HAS ENDED
   function () {
     // $(".logo").removeClass("fun-heading-always");
     // $(".fun-heading").removeClass("fun-heading-always");
@@ -36,8 +63,8 @@ $(".fun-heading").hover(
     // $(".b-or-w").removeClass("b-or-w-always");
   }
 );
-
-$("#hero").click(function () {
+// REMOVE COLORS WHEN HERO IS CLICKED
+$(".circle").click(function () {
   // $(".logo").removeClass("fun-heading-always");
   $(".fun-heading").removeClass("fun-heading-always");
   // $(".going-to-raise").removeClass("raise-up");
